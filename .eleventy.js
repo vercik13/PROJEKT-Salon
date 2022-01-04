62b4f68f0810238bdcdc73197f19acfe9ef409be
Veronika Vokounová <ver.cik@seznam.cz>
2022-01-04 22:25:06 +0100

    změna  fotogalerie

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function(eleventyConfig) {
 
  // Zkopírovat css/ to _site/css/
  eleventyConfig.addPassthroughCopy("css");
+ eleventyConfig.addPassthroughCopy("**/*.js");
 
  return {
     // možné formáty šablon
